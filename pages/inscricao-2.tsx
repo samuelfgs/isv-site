dabbd8368803e5c9d443c49978946d2b38a815f6
Samuel <samuel@plasmic.app>
2023-09-04 18:46:46 -0300

    remove isInternal

--- pages/inscricao-2.tsx
+++ pages/inscricao-2.tsx
@@ -41,7 +41,6 @@ function Inscricao2() {
           endpoint={process.env.NEXT_PUBLIC_ENDPOINT!}
           adultPrice={0.2}
           kidsPrice={0.1}
-          isInternal={true}
           emailValidator={validate}
         />
       </ph.PageParamsProvider>
