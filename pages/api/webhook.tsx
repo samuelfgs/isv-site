a247a42ffa50b3b925130c6fe65c5920fb7f00e6
Samuel <samuel@plasmic.app>
2023-09-20 14:25:03 -0300

    launch webhook

--- pages/api/webhook.tsx
+++ pages/api/webhook.tsx
@@ -5,87 +5,96 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!, { auth: { persistSession: false }});
 
+const testEndpoint = async (data: any) => {
+  fetch("https://eoukpp0g1g15gt3.m.pipedream.net", {
+    method: "POST",
+    body: data
+  });
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   console.log("dale", req.body, typeof req.body);
   try {
-    await fetch("https://eoukpp0g1g15gt3.m.pipedream.net", {
-      method: "POST",
-      body: JSON.stringify({ a: req.body, b: req.query, bd: typeof req.body })
-    });
-    res.status(200).json({success: true });
-    return;
+    if (req.body === "test") {
+      await testEndpoint(JSON.stringify({ a: req.body, b: req.query, bd: typeof req.body }));
+      res.status(200).json({success: true });
+      return;
+    }
   } catch (err) {
     console.log("dale", err);
     res.status(500).json(err);
     return;
   }
-  // const id = 22977802;
-  // // const id = req.body?.data?.id;
-  // const mercadoPago2 = await (await fetch(`https://api.mercadopago.com/v1/payments/${id}`, {
-  //   headers: {
-  //     authorization: `Bearer ${process.env.ACCESS_TOKEN}`
-  //   }
-  // })).json();
-  // console.log("dale", mercadoPago2);
-  // // if (mercadoPago.status === "approved") {
-  // // }
-  // // .update({ paid: true, method: mercadoPago.payment_method_id })
+  const id = req.body?.data?.id;  
+  const mercadoPago = await (await fetch(`https://api.mercadopago.com/v1/payments/${id}`, {
+    headers: {
+      authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+    }
+  })).json();
+  console.log("dale", id, mercadoPago);
+  await testEndpoint(JSON.stringify({ id, mercadoPago }));
+  if (mercadoPago.status !== "approved") {
+    res.status(500).json("not paid");
+    return;
+  }
+  const { data: paymentData, error: paymentError } = await supabase
+    .from('payments')
+    .update({ paid: true, method: mercadoPago.payment_method_id })
+    .eq('id', +mercadoPago.external_reference)
+    .select("*")
 
-  // const mercadoPago = { payment_method_id: "pix", external_reference: id };
-  // const { data: paymentData, error: paymentError } = await supabase
-  //   .from('payments')
-  //   .update({ paid: true, method: mercadoPago.payment_method_id })
-  //   .eq('id', +mercadoPago.external_reference)
-  //   .select("*")
-
-  // if (paymentError || !paymentData || paymentData.length !== 1) {
-  //   res.status(500).json({ paymentError, paymentData });
-  //   return ;
-  // }
+  if (paymentError || !paymentData || paymentData.length !== 1) {
+    res.status(500).json({ paymentError, paymentData });
+    return ;
+  }
   
-  // const { data, error } = await supabase
-  //   .from("inscritos")
-  //   .select("*")
-  //   .eq("id", paymentData[0].user_id)
+  const { data, error } = await supabase
+    .from("inscritos")
+    .select("*")
+    .eq("id", paymentData[0].user_id)
 
-  // if (error || !data || data.length !== 1) {
-  //   res.status(500).json({error, data});
-  //   return;
-  // }
-  // const inscrito = data[0];
-  // console.log("dale2", inscrito);
-  // try {
-  //   const email = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT}/api/email`, {
-  //     method: "POST",
-  //     body: JSON.stringify({
-  //       name: inscrito.name,
-  //       cpf: inscrito.cpf,
-  //       email: inscrito.email,
-  //       kids: paymentData[0].kids,
-  //       adultos: paymentData[0].adults,
-  //       price: paymentData[0].price,
-  //       id: `${inscrito.id}`
-  //     })
-  //   });
-  //   await email.json();
-  // } catch (err) {
-  //   console.log("dale3", err)
-  //   res.status(500).json(err);
-  //   return;
-  // }
+  if (error || !data || data.length !== 1) {
+    res.status(500).json({error, data});
+    return;
+  }
+  const inscrito = data[0];
+  if (inscrito.sent_email) {
+    res.status(200).json({...inscrito, already_sent: true});
+    return;
+  }
+  console.log("dale2", inscrito);
+  try {
+    const email = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT}/api/email`, {
+      method: "POST",
+      body: JSON.stringify({
+        name: inscrito.name,
+        cpf: inscrito.cpf,
+        email: inscrito.email,
+        kids: paymentData[0].kids,
+        adultos: paymentData[0].adults,
+        price: paymentData[0].price,
+        id: `${inscrito.id}`
+      })
+    });
+    await email.json();
+  } catch (err) {
+    console.log("dale3", err)
+    res.status(500).json(err);
+    return;
+  }
 
 
-  // const { data: data2, error: error2 } = await supabase
-  //   .from('inscritos')
-  //   .update({ sent_email: true })
-  //   .eq('id', inscrito.id)
+  const { error: error2 } = await supabase
+    .from('inscritos')
+    .update({ sent_email: true })
+    .eq('id', inscrito.id)
 
-  // if (error2) {
-  //   res.status(500).json(error2);
-  // } else {
-  //   res.status(200).json(inscrito);
-  // }
+  if (error2) {
+    res.status(500).json(error2);
+  } else {
+    res.status(200).json(inscrito);
+  }
 }
