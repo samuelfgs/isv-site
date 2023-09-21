076628369a1bae51fbfd50e407f9612e8d372e3e
Samuel <samuel@plasmic.app>
2023-09-21 14:23:17 -0300

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,16 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ParallaxProvider>
       <PlasmicRootProvider Head={Head}>
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-CZ6C4078E6"></script>
+          <script dangerouslySetInnerHTML={{ __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-CZ6C4078E6');
+          `}} />
+        </Head>
         <Component {...pageProps} />
       </PlasmicRootProvider>
     </ParallaxProvider>
